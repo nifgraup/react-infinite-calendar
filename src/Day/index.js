2c094ef70da1392ac12b4753fab6043247fc0095
Michael Vacík <miky@vivaldi.com>
2016-11-22 16:04:44 +0100

    Custom mouse event handlers
    • Custom mouse event handlers
    • Auto-scrolli guard (wip)
    • Formatting

--- src/Day/index.js
+++ src/Day/index.js
@@ -1,30 +1,51 @@
 import React from 'react';
 const style = require('./Day.scss');
 
-export default function Day({currentYear, date, day, handleDayClick, isDisabled, isToday, isSameMonth, isSelected, monthShort, locale, theme, renderMonthDay}) {
-	var {date: mmt, yyyymmdd} = date;
-	var year = mmt.year();
+export default function Day({currentYear, date, day, handleDayClick, isDisabled, isToday, isSameMonth, isSelected, monthShort, locale, theme, renderMonthDay, onDayMouseDown, onDayMouseUp, onDayMouseEnter}) {
+  var {date: mmt, yyyymmdd} = date;
+  var year = mmt.year();
 
-	return (
-		<li
-			className={`${style.root}${isToday ? ' ' + style.today : ''}${isSelected ? ' ' + style.selected : ''}${isDisabled ? ' ' + style.disabled : ' ' + style.enabled}`}
-			data-date={yyyymmdd}
-			onClick={(!isDisabled && handleDayClick) ? handleDayClick.bind(this, mmt) : null}
-		>
+  function handleEvent(handler, day) {
+    if (!handler) {
+      return undefined;
+    }
+    return (e) => {
+      e.persist();
+      handler(e, day);
+    };
+  }
+
+  return (
+    <li
+      className={`${style.root}${isToday ? ' ' + style.today : ''}${isSelected ? ' ' + style.selected : ''}${isDisabled ? ' ' + style.disabled : ' ' + style.enabled}`}
+      data-date={yyyymmdd}
+      onClick={(!isDisabled && handleDayClick && !onDayMouseDown)
+        ? handleDayClick.bind(this, mmt)
+        : null}
+      onMouseDown={onDayMouseDown && !isDisabled
+        ? handleEvent(onDayMouseDown, mmt)
+        : null}
+      onMouseUp={onDayMouseUp && !isDisabled
+        ? handleEvent(onDayMouseUp, mmt)
+        : null}
+      onMouseEnter={onDayMouseUp && !isDisabled
+        ? handleEvent(onDayMouseEnter, mmt)
+        : null}
+      >
       <span>{day}</span>
-      {renderMonthDay(mmt)}
+      {typeof renderMonthDay == 'function' && renderMonthDay(mmt)}
 
-			{(day === 1 && currentYear !== year) &&
+      {(day === 1 && currentYear !== year) &&
         <span className={style.year}>{year}</span>}
       {(day === 1) &&
         <span className={style.month}>{monthShort}</span>}
 
-			{/* {isSelected &&
-				<div className={style.selection} style={{backgroundColor: (typeof theme.selectionColor == 'function') ? theme.selectionColor(mmt) : theme.selectionColor, color: theme.textColor.active}}>
-					<span className={style.month}>{(isToday) ? (locale.todayLabel.short || locale.todayLabel.long) : monthShort}</span>
-					<span className={style.day}>{day}</span>
-				</div>
-			}  */}
-		</li>
-	);
+      {/* {isSelected &&
+        <div className={style.selection} style={{backgroundColor: (typeof theme.selectionColor == 'function') ? theme.selectionColor(mmt) : theme.selectionColor, color: theme.textColor.active}}>
+          <span className={style.month}>{(isToday) ? (locale.todayLabel.short || locale.todayLabel.long) : monthShort}</span>
+          <span className={style.day}>{day}</span>
+        </div>
+      }  */}
+    </li>
+  );
 }
--- src/Month/index.js
+++ src/Month/index.js
@@ -4,81 +4,84 @@ import Day from '../Day';
 const style = require('./Month.scss');
 
 export default class Month extends Component {
-	shouldComponentUpdate(nextProps) {
-		return (!nextProps.isScrolling && !this.props.isScrolling);
-	}
-	renderRows() {
-		let {disabledDates, disabledDays, displayDate, locale, maxDate, minDate, onDaySelect, rowHeight, rows, selectedDate, today, theme, renderMonthDay} = this.props;
-		let currentYear = today.date.year();
-		let monthShort = displayDate.format('MMM');
-		let monthRows = [];
-		let day = 0;
-		let isDisabled = false;
-		let isSelected = false;
-		let isToday = false;
+  shouldComponentUpdate(nextProps) {
+    return (!nextProps.isScrolling && !this.props.isScrolling);
+  }
+  renderRows() {
+    let {disabledDates, disabledDays, displayDate, locale, maxDate, minDate, onDaySelect, rowHeight, rows, selectedDate, today, theme, renderMonthDay, onDayMouseDown, onDayMouseUp, onDayMouseEnter} = this.props;
+    let currentYear = today.date.year();
+    let monthShort = displayDate.format('MMM');
+    let monthRows = [];
+    let day = 0;
+    let isDisabled = false;
+    let isSelected = false;
+    let isToday = false;
     let isSameMonth = false;
-		let row, date, days;
+    let row, date, days;
 
-		// Oh the things we do in the name of performance...
-		for (let i = 0, len = rows.length; i < len; i++) {
-			row = rows[i];
-			days = [];
+    // Oh the things we do in the name of performance...
+    for (let i = 0, len = rows.length; i < len; i++) {
+      row = rows[i];
+      days = [];
 
-			for (let k = 0, len = row.length; k < len; k++) {
-				date = row[k];
-				day++;
-				isSelected = (selectedDate && date.yyyymmdd == selectedDate.yyyymmdd);
+      for (let k = 0, len = row.length; k < len; k++) {
+        date = row[k];
+        day++;
+        isSelected = (selectedDate && date.yyyymmdd == selectedDate.yyyymmdd);
         isSameMonth = (selectedDate && date.yyyymmdd.substring(0, 6) ==
           selectedDate.yyyymmdd.substring(0, 5));
-				isToday = (today && date.yyyymmdd == today.yyyymmdd);
-				isDisabled = (
-					minDate && date.yyyymmdd < minDate.yyyymmdd ||
-					maxDate && date.yyyymmdd > maxDate.yyyymmdd ||
-					disabledDays && disabledDays.length && disabledDays.indexOf(date.date.day()) !== -1 ||
-					disabledDates && disabledDates.length && disabledDates.indexOf(date.yyyymmdd) !== -1
-				);
+        isToday = (today && date.yyyymmdd == today.yyyymmdd);
+        isDisabled = (
+          minDate && date.yyyymmdd < minDate.yyyymmdd ||
+          maxDate && date.yyyymmdd > maxDate.yyyymmdd ||
+          disabledDays && disabledDays.length && disabledDays.indexOf(date.date.day()) !== -1 ||
+          disabledDates && disabledDates.length && disabledDates.indexOf(date.yyyymmdd) !== -1
+        );
 
-				days[k] = (
-					<Day
-						key={`day-${day}`}
-						currentYear={currentYear}
-						date={date}
-						day={day}
-						handleDayClick={onDaySelect}
-						isDisabled={isDisabled}
-						isToday={isToday}
-						isSameMonth={isSameMonth}
-						isSelected={isSelected}
+        days[k] = (
+          <Day
+            key={`day-${day}`}
+            currentYear={currentYear}
+            date={date}
+            day={day}
+            handleDayClick={onDaySelect}
+            isDisabled={isDisabled}
+            isToday={isToday}
+            isSameMonth={isSameMonth}
+            isSelected={isSelected}
             renderMonthDay={renderMonthDay}
-						locale={locale}
-						monthShort={monthShort}
-						theme={theme}
-					/>
-				);
-			}
-			monthRows[i] = (
-				<ul className={classNames(style.row, {[style.partial]: row.length !== 7})} style={{height: rowHeight}} key={`Row-${i}`} role="row" aria-label={`Week ${i + 1}`}>
-					{days}
-				</ul>
-			);
-		}
+            onDayMouseDown={onDayMouseDown}
+            onDayMouseUp={onDayMouseUp}
+            onDayMouseEnter={onDayMouseEnter}
+            locale={locale}
+            monthShort={monthShort}
+            theme={theme}
+          />
+        );
+      }
+      monthRows[i] = (
+        <ul className={classNames(style.row, {[style.partial]: row.length !== 7})} style={{height: rowHeight}} key={`Row-${i}`} role="row" aria-label={`Week ${i + 1}`}>
+          {days}
+        </ul>
+      );
+    }
 
-		return monthRows;
-	}
-	render() {
-		let {displayDate, today, rows, showOverlay, rowStyle, theme} = this.props;
+    return monthRows;
+  }
+  render() {
+    let {displayDate, today, rows, showOverlay, rowStyle, theme} = this.props;
 
-		return (
-			<div className={style.root} style={rowStyle}>
-				<div className={style.rows}>
-					{this.renderRows()}
-					{showOverlay &&
-						<label className={classNames(style.label, {[style.partialFirstRow] : (rows[0].length !== 7)})} style={theme && theme.overlayColor && {backgroundColor: theme.overlayColor}}>
-							<span>{`${displayDate.format('MMMM')}${(!displayDate.isSame(today.date, 'year')) ? ' ' + displayDate.year() : ''}`}</span>
-						</label>
-					}
-				</div>
-			</div>
-		);
-	}
+    return (
+      <div className={style.root} style={rowStyle}>
+        <div className={style.rows}>
+          {this.renderRows()}
+          {showOverlay &&
+            <label className={classNames(style.label, {[style.partialFirstRow] : (rows[0].length !== 7)})} style={theme && theme.overlayColor && {backgroundColor: theme.overlayColor}}>
+              <span>{`${displayDate.format('MMMM')}${(!displayDate.isSame(today.date, 'year')) ? ' ' + displayDate.year() : ''}`}</span>
+            </label>
+          }
+        </div>
+      </div>
+    );
+  }
 }
